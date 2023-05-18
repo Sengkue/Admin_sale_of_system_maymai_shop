3aea1e386f883821620b989ad41ffa378927173f
sengkuevang blongmai <skvgithub@gmail.com>
2023-05-19 00:06:18 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,188 @@
 <template>
-  <div>
-    <!-- mobile -->
-    <div v-if="$vuetify.breakpoint.mobile">
-      <v-app>
-        <Nuxt />
-      </v-app>
-    </div>
-    <!-- web  -->
-    <v-app v-else class="font grey lighten-5">
-      <MainDesktop />
-      <v-main>
-      </v-main>
-    </v-app>
-  </div>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list v-for="(item, i) in items" :key="i" dense class="pt-0" tile flat>
+        <!-- Menu drop down -->
+        <v-list-group v-if="item.children" :prepend-icon="item.icon" no-action>
+        <!-- title name of topic -->
+          <template #activator>
+            <v-list-item-title class="py-1 ml-n5" style="font-weight: bold; font-size: 18px">{{
+              item.title
+            }}</v-list-item-title>
+          </template>
+        <!-- child lists menu display icon and title-->
+          <v-list-item
+            v-for="(child, j) in item.children"
+            :key="j"
+            :to="child.to"
+            router
+            exact
+            active-class="blue-grey lighten-4"
+          >
+         <!-- child list icon -->
+            <v-list-item-action class="ml-n6">
+              <v-icon>{{ child.icon }}</v-icon>
+            </v-list-item-action>
+         <!-- child list title -->
+            <v-list-item-title class="ml-n6" style="font-size: 15px">{{
+              child.title
+            }}</v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+        <!-- Menu title One line -->
+        <v-list-item
+          v-else
+          :to="item.to"
+          router
+          exact
+          dense
+          active-class="blue-grey lighten-4"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content class="ml-n5">
+            <v-list-item-title
+              class="py-1"
+              style="font-weight: bold; font-size: 18px"
+              >{{ item.title }}</v-list-item-title
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-avatar size="45" class="mr-1 ml-2" color="red">
+        <v-img class="ma-1" :src="require('@/static/images/logo.png')" />
+      </v-avatar>
+      <!-- <h2>Sale System Of Maymai Shop</h2> -->
+      <v-spacer></v-spacer>
+      <h2 style="color: #004d40">ລະບົບຂາຍ ຮ້ານຂາຍເຄື່ອງແມ່ໄໝ</h2>
+      <v-spacer />
+      <v-btn text color="primary" small><v-icon>mdi-login</v-icon></v-btn>
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+
+  </v-app>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      notifications: [],
+      dialog: false,
+      dialogLogOut: false,
+      clipped: true,
+      drawer: true,
+      fixed: false,
+      miniVariant: false,
+      rightDrawer: false,
+      items: [
+        {
+          icon: 'mdi-view-dashboard',
+          title: 'ໜ້າຫຼັກ',
+          to:"/",
+        },
+        //  ຈັດການຂໍ້ມູນພີ້ນຖາມ
+        {
+          icon: 'mdi-dots-triangle',
+          title: 'ຈັດການຂໍ້ມູນພື້ນຖານ',
+          children: [
+            {
+              icon: 'mdi-tshirt-crew',
+              title: 'ສິນຄ້າ',
+              to: { name: 'product' },
+            },
+            {
+              icon: 'mdi-shape',
+              title: 'ຂໍ້ມູນປະເພດ',
+              to: { name: 'category' },
+            },
+            {
+              icon: 'mdi-account-tie',
+              title: 'ຂໍ້ມູນພະນັກງານ',
+              to: { name: 'employee' },
+            },
+            {
+              icon: 'mdi-account-cowboy-hat',
+              title: 'ຂໍ້ມູນຜູ້ສະໜອງ',
+              to: { name: 'supplier' },
+            },
+            {
+              icon: 'mdi-account',
+              title: 'ຂໍ້ມູນລູກຄ້າ',
+              to: { name: 'customer' },
+            },
+            {
+              icon: 'mdi-folder-file',
+              title: 'ຈັດການໂປຼໂມຊັນ',
+              to: { name: 'promotion' },
+            },
+          ],
+        },
+        {
+          icon: 'mdi-message-arrow-right',
+          title: 'ສັ່ງຊື້ສິນຄ້າ',
+          to: 'order',
+        },
+        {
+          icon: 'mdi-table-arrow-left',
+          title: 'ນໍາເຂົ້າສິນຄ້າ',
+          to: 'import',
+        },
+        {
+          icon: 'mdi-widgets',
+          title: 'ຂາຍສິນຄ້າ',
+          to: 'sale',
+        },
+        //  ລາຍງານ
+        {
+          icon: 'mdi-chart-areaspline',
+          title: 'ລາຍງານ',
+          children: [
+            {
+              icon: 'mdi-tshirt-crew',
+              title: 'ຂໍ້ມູນສິນຄ້າ',
+              to: '/report/admin.product',
+            },
+            {
+              icon: 'mdi-chart-bell-curve',
+              title: 'ຂໍ້ມູນການຂາຍ',
+              to: '/report/admin.sale' ,
+            },
+            {
+              icon: 'mdi-folder-file',
+              title: 'ຂໍ້ມູນລູກຄ້າ',
+              to:'/report/admin.customer' ,
+            },
+            {
+              icon: 'mdi-folder-file',
+              title: 'ຂໍ້ມູນພະນັກງານ',
+              to:'/report/admin.employee',
+            },
+            {
+              icon: 'mdi-baby-carriage',
+              title: 'ຂໍ້ມູນການສັງຊື້',
+              to: '/report/admin.order' ,
+            },
+            // {
+            //   icon: 'mdi-folder-file',
+            //   title: 'ລາຍຮັບ',
+            //   to: { name: 'order' },
+            // },
+          ],
+        },
+      ],
+    }
   },
 }
 </script>
-
-<style scoped>
-/* * {
-  font-family: "Noto Serif Lao", serif;
-} */
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,245 +1,267 @@
 <template>
-  <div style="background: #333">
-    <v-card color="#fafafa" elevation="0">
-      <div class="d-flex justify-center mb-2 px-1 mt-5 x">
-        <v-card
-          v-ripple="{ class: `green--text` }"
-          elevation="2"
-          class="card"
-          style="background: linear-gradient(to right, #1b5e20, #00e676)"
-        >
-          <div class="d-flex">
-            <v-icon color="white" size="40">mdi-medical-bag</v-icon>
-            <v-divider vertical class></v-divider>
-            <div class="pa-2 white--text">
-              <h3 style="color: #e0f7fa">ຍອດຂາຍລວມທັງໝົດ</h3>
-              <span>12000 ກິບ</span>
-            </div>
-          </div>
-        </v-card>
-        <v-spacer></v-spacer>
-        <v-card
-          v-ripple="{ class: `green--text` }"
-          elevation="2"
-          class="card"
-          style="background: linear-gradient(120deg, #880e4f, #ff80ab)"
-        >
-          <div class="d-flex">
-            <v-icon color="white" size="40">mdi-table-arrow-left</v-icon>
-            <v-divider vertical class></v-divider>
-            <div class="pa-2 white--text">
-              <h3 style="color: #fbe9e7">ຍອດນໍາເຂົ້າ</h3>
-              <span>130000 ກິບ</span>
-            </div>
-          </div>
-        </v-card>
-        <v-spacer></v-spacer>
-        <v-card
-          v-ripple="{ class: `green--text` }"
-          elevation="2"
-          class="card"
-          style="background: linear-gradient(120deg, #1a237e, #82b1ff)"
-        >
-          <div class="d-flex">
-            <v-icon color="white" size="40">mdi-tshirt-crew</v-icon>
-            <v-divider vertical class></v-divider>
-            <div class="pa-2 white--text">
-              <h3 style="color: #f3e5f5">ສິນຄ້າ</h3>
-              <span> 56 ລາຍການ</span>
-            </div>
-          </div>
-        </v-card>
-        <v-spacer></v-spacer>
-        <v-card
-          v-ripple="{ class: `green--text` }"
-          elevation="2"
-          class="card"
-          style="background: linear-gradient(to right, #e65100, #ffcc80)"
-        >
-          <div class="d-flex">
-            <v-icon color="white" size="40">mdi-account-tie</v-icon>
-            <v-divider vertical class></v-divider>
-            <div class="pa-2 white--text">
-              <h3>ພະນັກງານ</h3>
-              <span>129 ຄົນ</span>
-            </div>
-          </div>
-        </v-card>
-        <v-spacer></v-spacer>
-        <v-card
-          v-ripple="{ class: `cyan--text` }"
-          elevation="2"
-          class="card"
-          style="background: linear-gradient(to right, #59859c, #c931eb)"
-        >
-          <div class="d-flex">
-            <v-icon color="white" size="40">mdi-account</v-icon>
-            <v-divider vertical class></v-divider>
-            <div class="pa-2 white--text">
-              <h3>ຕົວແທນ</h3>
-              <span>178 ຄົນ</span>
-            </div>
-          </div>
-        </v-card>
-      </div>
-      <div>
+  <div>
+    <v-container fluid>
+      <v-card elevation="0">
         <v-row>
-          <v-col cols="12" md="9">
-            <v-row no-gutters class="justify-center ma-1">
-              <v-hover v-slot="{ hover }" open-delay="200">
-                <v-card
-                  :elevation="hover ? 20 : 2"
-                  :class="{ 'on-hover': hover }"
-                  width="100%"
-                  height="400"
+          <v-col cols="12" sm="3" lg="3" v-ripple="{ class: `green--text` }">
+            <v-card
+              elevation="2"
+              class="white--text pa-2"
+              style="background: linear-gradient(to right, #1b5e20, #00e676)"
+            >
+              <div class="d-flex justify-space-between align-center">
+                <div
+                  class="py-2 text-caption text-sm-body-2 text-md-body-1 text-lg-h6"
                 >
-                  <v-toolbar dense flat>
-                    <v-spacer></v-spacer>
-                    <p></p>
-                    <v-spacer></v-spacer>
-                    <v-menu>
-                      <template #activator="{ on, attrs }">
-                        <div
-                          v-ripple="{ class: `green--text` }"
-                          class="d-flex"
-                          v-bind="attrs"
-                          v-on="on"
-                        >
-                          <v-icon color="">mdi-filter-variant</v-icon>
+                  <h3 style="color: #e0f7fa">ລາຍຮັບທັງໝົດ</h3>
+                  <span>120,000,000 ກິບ</span>
+                </div>
+                <div >
+                  <v-icon color="white" size="40">mdi-medical-bag</v-icon>
+                </div>
+              </div>
+              <v-divider class="white mt-n2 mb-1" ></v-divider>
+              <div class="d-flex">
+                <v-icon color="white">mdi-arrow-down-circle-outline</v-icon>
+                <div>Dialy Orders</div>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="3" lg="3" md="3">
+            <v-card
+              elevation="2"
+              v-ripple="{ class: `green--text` }"
+              class="white--text pa-2"
+              style="background: linear-gradient(120deg, #880e4f, #ff80ab)"
+            >
+            <div class="d-flex justify-space-between align-center">
+                <div
+                  class="py-2 text-caption text-sm-body-2 text-md-body-1 text-lg-h6"
+                >
+                  <h3 style="color: #e0f7fa">ລາຍຈ່າຍທັງໝົດ</h3>
+                  <span>50,000,000 ກິບ</span>
+                </div>
+                <div >
+                  <v-icon color="white" size="40">mdi-credit-card-multiple-outline</v-icon>
+                </div>
+              </div>
+              <v-divider class="white mt-n2 mb-1" ></v-divider>
+              <div class="d-flex">
+                <v-icon color="white">mdi-arrow-up-circle-outline</v-icon>
+                <div>Less Sales</div>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="3" lg="3">
+            <v-card
+              v-ripple="{ class: `green--text` }"
+              elevation="2"
+              class="white--text pa-2"
+              style="background: linear-gradient(120deg, #1a237e, #82b1ff)"
+            >
+            <div class="d-flex justify-space-between align-center">
+                <div
+                  class="py-2 text-caption text-sm-body-2 text-md-body-1 text-lg-h6"
+                >
+                  <h3 style="color: #e0f7fa">ກຳໄລທັງໝົດ</h3>
+                  <span>70,000,000 ກິບ</span>
+                </div>
+                <div >
+                  <v-icon color="white" size="40">mdi-chart-bar</v-icon>
+                </div>
+              </div>
+              <v-divider class="white mt-n2 mb-1" ></v-divider>
+              <div class="d-flex">
+                <v-icon color="white">mdi-exclamation-thick</v-icon>
+                <div>From Last MOnth</div>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="3" lg="3">
+            <v-card
+              v-ripple="{ class: `green--text` }"
+              elevation="2"
+              class="white--text pa-2"
+              style="background: linear-gradient(to right, #e65100, #ffcc80)"
+            >
+            <div class="d-flex justify-space-between align-center">
+                <div
+                  class="py-2 text-caption text-sm-body-2 text-md-body-1 text-lg-h6"
+                >
+                  <h3 style="color: #e0f7fa">ສະຕ໋ອກສິນຄ້າທັງໝົດ</h3>
+                  <span>500</span>
+                </div>
+                <div >
+                  <v-icon color="white" size="40">mdi-newspaper</v-icon>
+                </div>
+              </div>
+              <v-divider class="white mt-n2 mb-1" ></v-divider>
+              <div class="d-flex">
+                <v-icon color="white">mdi-check-decagram</v-icon>
+                <div>Recent History</div>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+        <div>
+          <v-row>
+            <v-col cols="12" md="9">
+              <v-row no-gutters class="justify-center ma-1">
+                <v-hover v-slot="{ hover }" open-delay="200">
+                  <v-card
+                    :elevation="hover ? 20 : 2"
+                    :class="{ 'on-hover': hover }"
+                    width="100%"
+                    height="400"
+                  >
+                    <v-toolbar dense flat>
+                      <v-spacer></v-spacer>
+                      <p></p>
+                      <v-spacer></v-spacer>
+                      <v-menu>
+                        <template #activator="{ on, attrs }">
+                          <div
+                            v-ripple="{ class: `green--text` }"
+                            class="d-flex"
+                            v-bind="attrs"
+                            v-on="on"
+                          >
+                            <v-icon color="">mdi-filter-variant</v-icon>
 
-                          <h3>ລາຍການ</h3>
-                        </div>
-                      </template>
+                            <h3>ລາຍການ</h3>
+                          </div>
+                        </template>
 
-                      <v-list>
-                        <v-list-item
-                          v-for="(item, index) in items"
-                          :key="index"
-                          link
-                        >
-                          <v-list-item-title @click="SwitchMenu(item.title)">
-                            {{ item.title }}
-                          </v-list-item-title>
-                        </v-list-item>
-                      </v-list>
-                    </v-menu>
+                        <v-list>
+                          <v-list-item
+                            v-for="(item, index) in items"
+                            :key="index"
+                            link
+                          >
+                            <v-list-item-title @click="SwitchMenu(item.title)">
+                              {{ item.title }}
+                            </v-list-item-title>
+                          </v-list-item>
+                        </v-list>
+                      </v-menu>
 
-                    <v-spacer></v-spacer>
-                    <v-card
-                      class="elevation-0 mt-1"
-                      color="transparent"
-                      width="100"
-                    >
-                      <v-select
-                        v-model="selectDate"
-                        :items="date_list"
-                        single-line
-                        placeholder="Date"
-                        hint="Date"
-                      ></v-select>
-                    </v-card>
-                  </v-toolbar>
+                      <v-spacer></v-spacer>
+                      <v-card
+                        class="elevation-0 mt-1"
+                        color="transparent"
+                        width="100"
+                      >
+                        <v-select
+                          v-model="selectDate"
+                          :items="date_list"
+                          single-line
+                          placeholder="Date"
+                          hint="Date"
+                        ></v-select>
+                      </v-card>
+                    </v-toolbar>
 
-                  <!-- <line-chart
+                    <!-- <line-chart
                       width="100%"
                       height="300"
                       v-if="list"
                       :chart-data="list"
                     ></line-chart> -->
-                  <v-row
-                    v-if="title == 'ກຣາຟສະແດງຍອດລວມ'"
-                    class="justify-space-around mt-3"
-                  >
-                    <span style="font-weight: bold; font-size: 15px"
-                      >ຍອດສັ່ງຕັດສິນຄ້າລວມ: ກິບ</span
+                    <v-row
+                      v-if="title == 'ກຣາຟສະແດງຍອດລວມ'"
+                      class="justify-space-around mt-3"
                     >
-                    <span style="font-weight: bold; font-size: 15px"
-                      >ຍອດສັ່ງຊື້ສິນຄ້າລວມ: ກິບ</span
-                    >
-                    <span style="font-weight: bold; font-size: 15px"
-                      >ຍອດຂາຍສິນຄ້າລວມ: ກິບ</span
-                    >
-                  </v-row>
-                  <v-row v-else class="justify-center mt-3">
-                    <span style="font-weight: bold; font-size: 25px"
-                      >ຍອດລວມ: ກິບ</span
-                    >
-                  </v-row>
+                      <span style="font-weight: bold; font-size: 15px"
+                        >ຍອດສັ່ງຕັດສິນຄ້າລວມ: ກິບ</span
+                      >
+                      <span style="font-weight: bold; font-size: 15px"
+                        >ຍອດສັ່ງຊື້ສິນຄ້າລວມ: ກິບ</span
+                      >
+                      <span style="font-weight: bold; font-size: 15px"
+                        >ຍອດຂາຍສິນຄ້າລວມ: ກິບ</span
+                      >
+                    </v-row>
+                    <v-row v-else class="justify-center mt-3">
+                      <span style="font-weight: bold; font-size: 25px"
+                        >ຍອດລວມ: ກິບ</span
+                      >
+                    </v-row>
+                  </v-card>
+                </v-hover>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="3">
+              <v-hover v-slot="{ hover }" open-delay="200">
+                <v-card
+                  class="ma-1"
+                  :elevation="hover ? 20 : 2"
+                  :class="{ 'on-hover': hover }"
+                  width="100%"
+                  height="400"
+                >
+                  <v-card-title primary-title>
+                    <v-row class="mx-2 justify-space-between">
+                      <p style="font-weight: bold">ຕົວແທນຕົວແບບ</p>
+                      <router-link to="/report_customer">
+                        <p color="green" text>View More</p>
+                      </router-link>
+                    </v-row>
+                  </v-card-title>
+                  <CustBarChart />
                 </v-card>
               </v-hover>
-            </v-row>
-          </v-col>
-          <v-col cols="12" md="3">
-            <v-hover v-slot="{ hover }" open-delay="200">
-              <v-card
-                class="ma-1"
-                :elevation="hover ? 20 : 2"
-                :class="{ 'on-hover': hover }"
-                width="100%"
-                height="400"
-              >
-                <v-card-title primary-title>
-                  <v-row class="mx-2 justify-space-between">
-                    <p style="font-weight: bold">ຕົວແທນຕົວແບບ</p>
-                    <router-link to="/report_customer">
-                      <p color="green" text>View More</p>
-                    </router-link>
-                  </v-row>
-                </v-card-title>
-                <CustBarChart />
-              </v-card>
-            </v-hover>
-          </v-col>
-        </v-row>
+            </v-col>
+          </v-row>
 
-        <v-row>
-          <v-col cols="12" class="justify-center" md="4">
-            <v-row no-gutters class="justify-center">
-              <v-col cols="12" md="4" class="align-center mx-5">
-                <v-hover v-slot="{ hover }" open-delay="200">
-                  <v-card
-                    :elevation="hover ? 20 : 2"
-                    :class="{ 'on-hover': hover }"
-                    width="100%"
-                    height="400"
-                  >
-                    <v-card-title primary-title>
-                      <v-row class="mx-2 justify-space-between">
-                        <p style="font-weight: bold">ສິນຄ້າທີ່ນິຍົມສັ່ງຊື້</p>
-                        <router-link to="/report">
-                          <p color="green" text>View More</p>
-                        </router-link>
-                      </v-row>
-                    </v-card-title>
-                  </v-card>
-                </v-hover>
-              </v-col>
+          <v-row>
+            <v-col cols="12" class="justify-center" md="4">
+              <v-row no-gutters class="justify-center">
+                <v-col cols="12" md="4" class="align-center mx-5">
+                  <v-hover v-slot="{ hover }" open-delay="200">
+                    <v-card
+                      :elevation="hover ? 20 : 2"
+                      :class="{ 'on-hover': hover }"
+                      width="100%"
+                      height="400"
+                    >
+                      <v-card-title primary-title>
+                        <v-row class="mx-2 justify-space-between">
+                          <p style="font-weight: bold">ສິນຄ້າທີ່ນິຍົມສັ່ງຊື້</p>
+                          <router-link to="/report">
+                            <p color="green" text>View More</p>
+                          </router-link>
+                        </v-row>
+                      </v-card-title>
+                    </v-card>
+                  </v-hover>
+                </v-col>
 
-              <v-col cols="12" md="4">
-                <v-hover v-slot="{ hover }" open-delay="200">
-                  <v-card
-                    :elevation="hover ? 20 : 2"
-                    :class="{ 'on-hover': hover }"
-                    width="100%"
-                    height="400"
-                  >
-                    <v-card-title>
-                      <v-row class="mx-2 justify-space-between">
-                        <p style="font-weight: bold">ສິນຄ້າທີ່ໄດ້ຂາຍດີ</p>
-                        <router-link to="/report_production">
-                          <p color="green" text>View More</p>
-                        </router-link>
-                      </v-row>
-                    </v-card-title>
-                    <v-card-subtitle>5 ລາຍການ</v-card-subtitle>
-                  </v-card>
-                </v-hover>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-      </div>
-    </v-card>
+                <v-col cols="12" md="4">
+                  <v-hover v-slot="{ hover }" open-delay="200">
+                    <v-card
+                      :elevation="hover ? 20 : 2"
+                      :class="{ 'on-hover': hover }"
+                      width="100%"
+                      height="400"
+                    >
+                      <v-card-title>
+                        <v-row class="mx-2 justify-space-between">
+                          <p style="font-weight: bold">ສິນຄ້າທີ່ໄດ້ຂາຍດີ</p>
+                          <router-link to="/report_production">
+                            <p color="green" text>View More</p>
+                          </router-link>
+                        </v-row>
+                      </v-card-title>
+                      <v-card-subtitle>5 ລາຍການ</v-card-subtitle>
+                    </v-card>
+                  </v-hover>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </div>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
@@ -264,10 +286,8 @@ export default {
 </script>
 <style scoped>
 .card {
-  max-width: 100%;
-  width: 100%;
-  min-width: 240px;
-  margin: 7px 2px;
-  padding: 5px 3px;
+  margin-top: -10px;
+  padding-left: -10px;
+  padding-right: -2px;
 }
 </style>
